506b544f6192fba91f8a7af942c22f2e99c8a33f
Ahmed-mahd <ahmedmahmoudyousef@gmail.com>
2025-05-20 01:02:51 +0300

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,26 @@
+const defaultTheme = require('tailwindcss/defaultTheme');
+const forms = require('@tailwindcss/forms');
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: [
+        './resources/**/*.blade.php',
+        './resources/**/*.js',
+        './resources/**/*.jsx',
+        './resources/**/*.vue',
+    ],
+
+    theme: {
+        extend: {
+            fontFamily: {
+                sans: ['Figtree', ...defaultTheme.fontFamily.sans],
+            },
+            colors: {
+                primary: '#3B82F6',
+                'primary-dark': '#2563EB',
+            },
+        },
+    },
+
+    plugins: [forms],
+};
